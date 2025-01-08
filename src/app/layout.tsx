ec1ed40df7ede55fd66884a6836ea9a7a2f6c2a2
harshit1602 <73531429+harshit1602@users.noreply.github.com>
2025-01-08 10:44:22 +0530

    integrating google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "SitesforUX",
@@ -36,6 +37,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HQ415ENF9Z"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HQ415ENF9Z');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
